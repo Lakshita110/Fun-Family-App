8b2472c6db5272b2157ec6a1357c65b5b94c02a5
Lakshita Jain <Lak.Jai110@gmail.com>
2020-11-04 20:51:25 +0100

    Database & heroku setup

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
+# from flask_session import Session 
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+# sess = Session()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    with app.app_context():
+        db.init_app(app)
+        # from .models import 
+        db.create_all()
+        login_manager.init_app(app)
+        # sess.init_app(app)    
+
+    return app
+
+app = create_app()
