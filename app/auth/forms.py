465698b804c5d2b70edd927e4f53bef6fe471d7a
Lakshita Jain <Lak.Jai110@gmail.com>
2020-11-08 10:42:28 +0100

    Updated models and forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,6 @@
+from logging import PlaceHolder
+from re import L
+from flask.signals import message_flashed
 from flask_wtf import FlaskForm
 from wtforms import (BooleanField, IntegerField, PasswordField, SelectField, StringField, SubmitField, TextAreaField)
 from wtforms.validators import (DataRequired, Email, EqualTo, Length, NumberRange, Optional, ValidationError)
@@ -6,12 +9,12 @@ from app import db
 
 # Form for registration displayed on register.html.
 class RegisterForm(FlaskForm):    
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25, message="Must be between 4 and 25 characters")])
     fullname = StringField('Full Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=6, max=35, message="Must be between 6 and 35 characters")])
     password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    existing_family = BooleanField('Existing family?')
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password', message="Passwords must match")])
+    existing_family = BooleanField('Family already exists')
     family_name = StringField('Family Name', validators=[DataRequired()])
     family_password = PasswordField('Family Password', validators=[DataRequired()])
     submit = SubmitField('Register')
@@ -26,10 +29,10 @@ class RegisterForm(FlaskForm):
         if user is not None:
             self.email.errors.append('Please use a different email address.')    
 
-    def validate_family_name(self, family_name):
+    """def validate_family_name(self, family_name):
         family = Family.query.filter_by(name=family_name.data).first()
         if family is not None:
-            self.family_name.errors.append('Please use a different family name.')
+            self.family_name.errors.append('Please use a different family name.')"""
     
 
 # Form for logging in displayed on login.html.
--- app/models.py
+++ app/models.py
@@ -1,12 +1,10 @@
 from flask_login import UserMixin
+from sqlalchemy.sql.elements import False_
 from werkzeug.security import check_password_hash, generate_password_hash
 from app import db, login_manager
 from datetime import datetime
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
+# User class for each individual user
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
     __table_args__ = {'extend_existing': True} 
@@ -17,8 +15,9 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     family_id = db.Column(db.Integer, db.ForeignKey('family._id'))
-    items = db.relationship('Item', backref='user')
 
+    family = db.relationship("Family", back_populates="users")
+    
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -27,11 +26,12 @@ class User(db.Model, UserMixin):
 
     def check_family_password(self, family_name, family_password):
         family = Family.query.filter_by(name=family_name).first()
-        return family.check_password(family_password)
+        if family is not None:
+            return family.check_password(family_password)
 
     def set_family_id(self, family_name):
         family = Family.query.filter_by(name=family_name).first()
-        self.family_id = family._id
+        family.users.append(self)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
@@ -39,6 +39,11 @@ class User(db.Model, UserMixin):
     def get_id(self):
         return (self._id)
 
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+# Family class for each family which can have several users
 class Family(db.Model):
     __tablename__ = 'family'
     __table_args__ = {'extend_existing': True} 
@@ -46,8 +51,9 @@ class Family(db.Model):
     _id = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String(120), unique=True)
     password_hash = db.Column(db.String(128))
-    members = db.relationship('User', backref='family')
-    lists = db.relationship('List', backref='family')
+
+    users = db.relationship("User", back_populates="family")
+    lists = db.relationship("List", back_populates="family")
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -67,19 +73,26 @@ class Family(db.Model):
         db.session.add(to_do_list)
         db.session.commit()
 
+# Lists class for lists which belong to one family
 class List(db.Model):
     __tablename__='list'
     __table_args__ = {'extend_existing': True} 
 
     _id = db.Column(db.Integer, primary_key=True, unique=True)
-    family_id = db.Column(db.Integer, db.ForeignKey('family._id'))
     category = db.Column(db.String(120))
-    items = db.relationship('Item', backref='list')
+    family_id = db.Column(db.Integer, db.ForeignKey("family._id"))
+    
+    family = db.relationship("Family", back_populates="lists")
+    items = db.relationship("Item", back_populates="list")
+    
+    def get_id(self):
+        return self._id
 
     def set_family_id(self, family_name):
         family = Family.query.filter_by(name=family_name).first()
         self.family_id = family._id
 
+# Items class for items which belong to one list
 class Item(db.Model):
     __tablename__ = 'item'
     __table_args__ = {'extend_existing': True} 
@@ -88,7 +101,11 @@ class Item(db.Model):
     value = db.Column(db.String(120))
     assigned_to = db.Column(db.Integer, db.ForeignKey('user._id'))
     created_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
-    list_id = db.Column(db.Integer, db.ForeignKey('list._id'))
+    complete = db.Column(db.Boolean, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey("list._id"))
+
+    list = db.relationship("List", back_populates="items")
+
 
 
 
